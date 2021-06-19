98121c1df693dadde6c98fab0f9e9f80c078b466
Edward Lee <d237lee@yandex.ru>
2021-06-19 18:23:51 +0500

    user creating

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from '@app/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from '@app/app.controller';
 import { TagModule } from '@app/tag/tag.module';
+import { UserModule } from '@app/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TagModule],
+  imports: [TypeOrmModule.forRoot(), TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
