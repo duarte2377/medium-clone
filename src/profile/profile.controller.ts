1de51b997c2b09cdb2ca3d9f1210d660dec318f3
Edward Lee <d237lee@yandex.ru>
2021-06-28 20:34:35 +0500

    get profile

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from '@app/user/decorators/user.decorator';
+import { ProfileService } from '@app/profile/profile.service';
+import { ProfileResponseInterface } from '@app/profile/types/profileResponse.interface';
+
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Get(':username')
+  public async getUserProfile(
+    @User('id') currentUserId: number,
+    @Param('username') username: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(
+      currentUserId,
+      username,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+}
